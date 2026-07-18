09e735125ea694b99a472c6e107d41f2111fbcc7
HarryMonty <harrymonty@live.com.au>
2026-07-18 15:38:56 +1000

    Bash script learning 1

---
+++ parrot-beginner-01-tool-baseline/bash-script/script.sh
@@ -0,0 +1,4 @@
+#This is an example bash script, used to automate commands. The script is run using "bash script.sh"
+
+mkdir folder1 folder2 # This will create 2 directory folders where the bash script is run
+
